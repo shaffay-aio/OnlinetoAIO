853a2a397ba87e8697426362a29de6bb15bf614b
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-02-11 10:36:25 +0500

    sequence based id assigning enabled

--- competitor/online/online_to_aio.py
+++ competitor/online/online_to_aio.py
@@ -72,7 +72,7 @@ def process_online(filename, platform):
         merged_df[i] = merged_df[i].apply(process_value)
 
     # if someone tests file via csv, its easier if ids are in sequence for particular category, item, modifier
-    #merged_df = merged_df.sort_values(by=['Category Name', 'Item Name', 'Modifier Name', 'Option Name'])
+    merged_df = merged_df.sort_values(by=['Category Name', 'Item Name', 'Modifier Name', 'Option Name'])
 
     for i in ['Category Name']:
         merged_df = assign_unique_ids(merged_df, i)
@@ -93,7 +93,7 @@ def assigner(aio_format, merged_df):
     # assign individual data
     aio_format['Category'][['id', 'categoryName']] = merged_df[['Category Name id', 'Category Name']].dropna().drop_duplicates()
 
-    # ISSUE: as an item had same price, but with different categories it had different description
+    # ISSUE TODO: as an item had same price, but with different categories it had different description
     # when it passes through UberEats, it uses assign_linked_ids so tehy get assigned same ids
     # when it passes through missing field it reassigns ids based on separate row
     # merged_df[['Item Name id', 'Item Name', 'Item Price']] = merged_df[['Item Name id', 'Item Name', 'Item Price']].drop_duplicates()
